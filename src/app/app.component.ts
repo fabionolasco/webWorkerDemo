b02e0a08b4fd41731e864462482ebe855f87b65b
Fabio Nolasco <fabio.nolasco@aig.com>
2018-08-15 16:27:47 -0400

    Added Web Workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,17 @@ export class AppComponent {
   public dogAge = 0;
 
   constructor() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/assets/sw.js').then((worker: any) => {
-        this.worker = worker;
-      });
+    if (window['Worker']) {
+      this.worker = new Worker('/assets/sw.js');
+      this.worker.onmessage = resp => {
+        console.log('resp', resp);
+        this.dogAge = resp.data['dogAge'];
+      };
     }
   }
 
   getDogAge() {
-    this.sendReceiveMsg({ 'age': this.humanAge })
-      .then(resp => {
-        console.log('resp', resp);
-        this.dogAge = resp['dogAge'];
-      });
-  }
-
-  sendReceiveMsg(msg) {
-    return new Promise((resolve, reject) => {
-      const msg_chan = new MessageChannel();
-      msg_chan.port1.onmessage = (event) => {
-        if (event.data.error) {
-          reject(event.data.error);
-        } else {
-          resolve(event.data);
-        }
-      };
-      this.worker.active.postMessage(msg, [msg_chan.port2]);
-    });
+    this.worker.postMessage({ 'age': this.humanAge });
   }
 
 }
--- src/assets/sw.js
+++ src/assets/sw.js
@@ -2,8 +2,8 @@ const dogAge = (data) => {
   return ((data.age / 10) * 2) - ((data.age / 57.7) * 0.921);
 };
 
-self.addEventListener('message', event => {
+onmessage = event => {
   console.log('event', event);
   const response = dogAge(event.data);
-  event.ports[0].postMessage({ 'dogAge': response });
-});
+  postMessage({ 'dogAge': response });
+};
